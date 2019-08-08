fb4c0a08870e85c003bd03efe0918e858b84249c
GuilhermArauj0oSilva <khav.prfs@gmail.com>
2019-08-08 13:53:30 -0300

    clone geral

--- calculadora/src/components/Button.jsx
+++ calculadora/src/components/Button.jsx
@@ -2,4 +2,4 @@ import React from 'react'
 import './Button.css'
 
 export default props =>
-    <button className='Button'>0</button>
+<button><a href="https://livecounts.net/channel/Freaks%20Killer">Inscritos no Meu Canal no YouTube</a></button>
